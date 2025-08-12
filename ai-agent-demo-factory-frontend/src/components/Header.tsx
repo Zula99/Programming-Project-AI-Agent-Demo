eb45ad418c9d103df41dd54aaa028e7a242739ef
MHN-0608 <Minhhoangnguyen0608@gmail.com>
2025-08-12 13:03:59 +1000

    Created base Header component

---
+++ ai-agent-demo-factory-frontend/src/components/Header.tsx
@@ -0,0 +1,11 @@
+
+export default function Header() {
+
+    return (
+        <nav className="bg-gray-800 text-white p-2 mt-auto">
+            <div className="w-full mx-auto max-w-screen-xl p-1">
+            AI Agent Demo
+            </div>
+        </nav>
+    );
+}
