01628e04a2abf65ba2abfbda82f9fd9c7b742f74
MHN-0608 <Minhhoangnguyen0608@gmail.com>
2025-08-12 13:03:42 +1000

    Created base Footer component

---
+++ ai-agent-demo-factory-frontend/src/components/Footer.tsx
@@ -0,0 +1,16 @@
+export default function Footer() {
+
+    return (
+        <footer className="bg-gray-800 text-white p-2 mt-auto">
+            <div className="w-full mx-auto max-w-screen-xl p-1">
+            Programming Project - AI Agent Demo
+            <ul>Curtis Brecko</ul>
+            <ul>Emily Robers</ul>
+            <ul>Maxim Iliagouev</ul>
+            <ul>Minh Nguyen</ul>
+            <ul>Yuchen Fang</ul>
+            
+            </div>
+        </footer>
+    );
+}
