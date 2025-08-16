0db69e0146c03a74c82f4968379c274f46a459cc
MHN-0608 <Minhhoangnguyen0608@gmail.com>
2025-08-16 18:08:58 +1000

    Adjusted name for http origin in main.py

--- ai-agent-demo-factory-backend/main.py
+++ ai-agent-demo-factory-backend/main.py
@@ -14,7 +14,7 @@ app = FastAPI()
 origins = [
     "http://localhost",
     "http://localhost:3000", 
-    "http://127.0.0.1:5000" # Explicitly allow self, if needed for some tests
+    "http://localhost:5000" # Explicitly allow self, if needed for some tests
 ]
 
 app.add_middleware(
